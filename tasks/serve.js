0a07027d639cbda3a1eb2304da2c08b56f92491a
Max Donchenko <wereya2@yandex.ru>
2016-07-25 15:23:20 +0300

    Gulp: add and optimize tasks

---
+++ tasks/serve.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const gulp = require('gulp');
+const webserver = require('gulp-webserver');
+
+module.exports = function(options) {
+
+    return function () {
+        gulp.src('app')
+            .pipe(webserver({
+                livereload: true,
+                // directoryListing: true,
+                open: true,
+                fallback: options.fallback
+            }));
+    };
+};
+
+// app.get('/', function(req, res, next) {
+//     res.send("Hello world");
+// });
---
+++ tasks/styles.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const $ = require('gulp-load-plugins')();
+const gulp = require('gulp');
+const combine = require('stream-combiner2').obj;
+
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+
+module.exports = function(options) {
+    return function() {
+        return combine(
+            gulp.src(options.src),
+            $.if(isDevelopment, $.sourcemaps.init()),
+            $.sass(),
+            $.if(isDevelopment, $.sourcemaps.write()),
+            gulp.dest('./app/css/')
+        );
+    };
+};
