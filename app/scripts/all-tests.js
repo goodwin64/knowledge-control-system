e90678fdb1b1ee01a7c64a22d16e63e7522be419
Max Donchenko <wereya2@yandex.ru>
2016-08-08 18:12:23 +0300

    Task-commn (12): tests sort

--- app/scripts/all-tests.js
+++ app/scripts/all-tests.js
@@ -1,15 +1,50 @@
+var jqCount = 0; // TODO: remove on production
+
 (function() {
+    // jQuery "contains" case ignore
     jQuery.expr[':'].contains = function(a, i, m) {
         return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
     };
 
-    document.getElementById("search-test").oninput = function() {
+    // on filter (test search)
+    document.getElementById("test-search").oninput = function() {
         var userInput = this.value;
         // var testList = document.getElementsByClassName("test-params");
         var testList = $(".list-group-item");
+        jqCount++;
         testList
             .css("display", "none")
             .filter(":contains("+userInput+")")
             .css("display", "list-item");
-    }
+    };
+
+    // on sort
+    document.getElementById("test-sort").onchange = sortTestItems;
+    document.getElementById("by-increasing").onchange = sortTestItems;
 })();
+
+function sortTestItems() {
+    var criteria = $("#test-sort").val();
+    jqCount++;
+
+    var mylist = $('.list-group');
+    var listitems = mylist.children('li').get();
+    listitems.sort(function(a, b) {
+        var compA = $(a).find(".test-" + criteria).text().toUpperCase();
+        var compB = $(b).find(".test-" + criteria).text().toUpperCase();
+        jqCount += 2;
+        if (criteria == "duration" || criteria == "complexity") {
+            compA = +compA;
+            compB = +compB;
+        }
+        var result = (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+
+        if (!$("#by-increasing").prop("checked")) {
+            result *= -1;
+        }
+
+        return result;
+    });
+    $.each(listitems, function(idx, itm) { mylist.append(itm); });
+    console.log("jQuery calls: ", jqCount);
+}
--- public/scripts/all-tests.js
+++ public/scripts/all-tests.js
@@ -1,10 +1,40 @@
+var jqCount = 0;
+
 (function() {
     jQuery.expr[":"].contains = function(a, i, m) {
         return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
     };
-    document.getElementById("search-test").oninput = function() {
+    document.getElementById("test-search").oninput = function() {
         var userInput = this.value;
         var testList = $(".list-group-item");
+        jqCount++;
         testList.css("display", "none").filter(":contains(" + userInput + ")").css("display", "list-item");
     };
+    document.getElementById("test-sort").onchange = sortTestItems;
+    document.getElementById("by-increasing").onchange = sortTestItems;
 })();
+
+function sortTestItems() {
+    var criteria = $("#test-sort").val();
+    jqCount++;
+    var mylist = $(".list-group");
+    var listitems = mylist.children("li").get();
+    listitems.sort(function(a, b) {
+        var compA = $(a).find(".test-" + criteria).text().toUpperCase();
+        var compB = $(b).find(".test-" + criteria).text().toUpperCase();
+        jqCount += 2;
+        if (criteria == "duration" || criteria == "complexity") {
+            compA = +compA;
+            compB = +compB;
+        }
+        var result = compA < compB ? -1 : compA > compB ? 1 : 0;
+        if (!$("#by-increasing").prop("checked")) {
+            result *= -1;
+        }
+        return result;
+    });
+    $.each(listitems, function(idx, itm) {
+        mylist.append(itm);
+    });
+    console.log("jQuery calls: ", jqCount);
+}
